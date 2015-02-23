c10ecf19e20218982e91811b2e139184860c6ced
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-23 13:15:29 +0100

    Initial commit

---
+++ src/FSharpx.Async/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharpx.Async")>]
+[<assembly: AssemblyProductAttribute("FSharpx.Async")>]
+[<assembly: AssemblyDescriptionAttribute("Async extensions for F#")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
