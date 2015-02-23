15067fd545724d4cc7a5243bd8e26bce1d593191
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-02-23 13:37:29 +0100

    Bump version to 1.9.6

--- src/FSharpx.Async/AssemblyInfo.fs
+++ src/FSharpx.Async/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpx.Async")>]
 [<assembly: AssemblyProductAttribute("FSharpx.Async")>]
 [<assembly: AssemblyDescriptionAttribute("Async extensions for F#")>]
-[<assembly: AssemblyVersionAttribute("1.0")>]
-[<assembly: AssemblyFileVersionAttribute("1.0")>]
+[<assembly: AssemblyVersionAttribute("1.9.6")>]
+[<assembly: AssemblyFileVersionAttribute("1.9.6")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.0"
+    let [<Literal>] Version = "1.9.6"
