3a413f98abec8ae550e7b48ec20c57c41b09e37b
mavnn <michael@mavnn.co.uk>
2015-02-26 11:16:21 +0000

    Removed a space.

--- src/FSharpx.Async/AssemblyInfo.fs
+++ src/FSharpx.Async/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharpx.Async")>]
 [<assembly: AssemblyProductAttribute("FSharpx.Async")>]
 [<assembly: AssemblyDescriptionAttribute("Async extensions for F#")>]
-[<assembly: AssemblyVersionAttribute("1.9.10")>]
-[<assembly: AssemblyFileVersionAttribute("1.9.10")>]
+[<assembly: AssemblyVersionAttribute("1.10.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.10.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.9.10"
+    let [<Literal>] Version = "1.10.0"
