1f4666bb5e12c9efd62c35aeecfac5b6db3fafed
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2015-03-27 08:42:39 +0100

    Bump version to 1.12.0

--- src/AsyncSeq/AssemblyInfo.fs
+++ src/AsyncSeq/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("AsyncSeq")>]
 [<assembly: AssemblyProductAttribute("AsyncSeq")>]
 [<assembly: AssemblyDescriptionAttribute("Asynchronous sequences for F#")>]
-[<assembly: AssemblyVersionAttribute("1.11.1")>]
-[<assembly: AssemblyFileVersionAttribute("1.11.1")>]
+[<assembly: AssemblyVersionAttribute("1.12.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.12.0")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.11.1"
+    let [<Literal>] Version = "1.12.0"
