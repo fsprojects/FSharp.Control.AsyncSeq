c81c452257ef75c9210632754000073cdbc0459a
Don Syme <donsyme@fastmail.fm>
2015-03-27 17:48:19 +0000

    fix doc links

--- src/FSharp.Control.AsyncSeq/AssemblyInfo.fs
+++ src/FSharp.Control.AsyncSeq/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Control.AsyncSeq")>]
 [<assembly: AssemblyProductAttribute("FSharp.Control.AsyncSeq")>]
 [<assembly: AssemblyDescriptionAttribute("Asynchronous sequences for F#")>]
-[<assembly: AssemblyVersionAttribute("1.12.2")>]
-[<assembly: AssemblyFileVersionAttribute("1.12.2")>]
+[<assembly: AssemblyVersionAttribute("1.13")>]
+[<assembly: AssemblyFileVersionAttribute("1.13")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "1.12.2"
+    let [<Literal>] Version = "1.13"
