938f79c4530fdcb3b86812f541e945dd748c3dc0
Craig Wilson <craig@wilsoncg.net>
2019-06-09 14:10:32 +0100

    Revert "bump version"
    This reverts commit 47f138d112e540e9e79f64a406b034bbc4168e9f.

--- src/FSharp.Control.AsyncSeq/AssemblyInfo.fs
+++ src/FSharp.Control.AsyncSeq/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("FSharp.Control.AsyncSeq")>]
 [<assembly: AssemblyProductAttribute("src/FSharp.Control.AsyncSeq")>]
 [<assembly: AssemblyDescriptionAttribute("Asynchronous sequences for F#")>]
-[<assembly: AssemblyVersionAttribute("2.0.22")>]
-[<assembly: AssemblyFileVersionAttribute("2.0.22")>]
+[<assembly: AssemblyVersionAttribute("2.0.21")>]
+[<assembly: AssemblyFileVersionAttribute("2.0.21")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "FSharp.Control.AsyncSeq"
     let [<Literal>] AssemblyProduct = "src/FSharp.Control.AsyncSeq"
     let [<Literal>] AssemblyDescription = "Asynchronous sequences for F#"
-    let [<Literal>] AssemblyVersion = "2.0.22"
-    let [<Literal>] AssemblyFileVersion = "2.0.22"
+    let [<Literal>] AssemblyVersion = "2.0.21"
+    let [<Literal>] AssemblyFileVersion = "2.0.21"
