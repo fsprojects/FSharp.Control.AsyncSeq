b8e6f9aebb10bcfe3eb51c7a71942eb099beb6aa
Craig Wilson <craig@wilsoncg.net>
2020-05-27 18:45:11 +0100

    mono build fix

--- build.fsx
+++ build.fsx
@@ -1,4 +1,4 @@
-// --------------------------------------------------------------------------------------
+﻿// --------------------------------------------------------------------------------------
 // FAKE build script
 // --------------------------------------------------------------------------------------
 
@@ -78,10 +78,10 @@ Target "AssemblyInfo" (fun _ ->
           Attribute.FileVersion release.AssemblyVersion ]
 
     let getProjectDetails projectPath =
-        let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath)
+        let projectName = System.IO.Path.GetFileNameWithoutExtension(projectPath: string)
         ( projectPath,
           projectName,
-          System.IO.Path.GetDirectoryName(projectPath),
+          System.IO.Path.GetDirectoryName(projectPath: string),
           (getAssemblyInfoAttributes projectName)
         )
 
