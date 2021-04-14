f7323ea1a04c26d5c0293e391d9f04364e900f3d
Don Syme <dsyme@users.noreply.github.com>
2021-04-14 14:17:52 +0100

    Update AsyncSeq.fs

--- src/FSharp.Control.AsyncSeq/AsyncSeq.fs
+++ src/FSharp.Control.AsyncSeq/AsyncSeq.fs
@@ -1746,9 +1746,9 @@ module AsyncSeq =
          return Choice1Of2 (asyncSeq { for v in res do yield v })
        }
 
+  #if !FABLE_COMPILER
   #if (NETSTANDARD2_1 || NETCOREAPP3_0)
 
-  #if !FABLE_COMPILER
   let ofAsyncEnum (source: Collections.Generic.IAsyncEnumerable<_>) = asyncSeq {
       let! ct = Async.CancellationToken
       let e = source.GetAsyncEnumerator(ct)
