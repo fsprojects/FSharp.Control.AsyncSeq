783606b7d643f9683a413782d79a131cd4b72219
APLEX\zoem <35312807+Xyncgas@users.noreply.github.com>
2022-10-13 20:08:38 -0700

    Implemented mapAsyncUnorderedParallel

---
+++ Feature/IAsyncEnumerableInterOp.fs
@@ -0,0 +1,25 @@
+﻿[<AutoOpen>]
+module IAsyncEnumerableInterOp
+    open System
+    open FSharp.Control
+    let toAsyncSeq<'t> (xs:System.Collections.Generic.IAsyncEnumerable<'t>) =
+        {new FSharp.Control.IAsyncEnumerable<'t> with
+            member _.GetEnumerator() =        
+                let aeEnum = xs.GetAsyncEnumerator()
+                {
+                    new FSharp.Control.IAsyncEnumerator<'t> with 
+                        member _.MoveNext() = 
+                            async {
+                                let! haveData = aeEnum.MoveNextAsync().AsTask() |> Async.AwaitTask
+                                if haveData then return Some aeEnum.Current else return None
+                            }
+
+                    interface IDisposable with
+                        member _.Dispose() = 
+                            aeEnum.DisposeAsync().AsTask() 
+                            |> Async.AwaitTask                            
+                            |> Async.RunSynchronously
+                }
+        }
+
+    let AsAsyncSeq (source:seq<'a>) = asyncSeq { for i in source do i}
---
+++ Feature/Library.fs
@@ -0,0 +1,37 @@
+﻿[<AutoOpen>]
+module ChannelExtention
+    open System.Threading
+    open System.Threading.Channels
+    open FSharp.Control
+
+    open LanguagePrimitives
+
+    let inline (|AbsPosRangeByLength|_|) (start,length) = if start >= GenericZero && length > GenericZero then Some (start,length) else None
+    let inline private AsyncSequenceProduce (dataChannel:Channel<'a>) producer source =
+        async{
+            let Mutex = new Mutex();
+            let mutable Completed = GenericZero;
+            let mutable Schedueled = GenericZero;
+            let mutable HasElement = false;
+            source
+            |> AsyncSeq.iter (fun x->
+                HasElement <- true
+                Schedueled <- Checked.(+) Schedueled GenericOne
+                let lc_x = x
+                async{
+                    try
+                        let! value = producer(lc_x)
+                        let! _ = dataChannel.Writer.WriteAsync(value).AsTask() |> Async.AwaitTask
+                        ()
+                    finally
+                        Mutex.WaitOne() |> ignore
+                        Completed <- Completed + GenericOne
+                        Mutex.ReleaseMutex()
+                        if Completed = Schedueled then dataChannel.Writer.Complete()
+                } |> Async.Start)
+            |> Async.RunSynchronously
+            if not HasElement then dataChannel.Writer.Complete()
+        } |> Async.Start
+        dataChannel.Reader.ReadAllAsync() |> toAsyncSeq
+
+    let mapAsyncUnorderedParallel transformer sequence = (AsyncSequenceProduce (Helper.CreateChannel.CreateUnbounded())) transformer sequence
